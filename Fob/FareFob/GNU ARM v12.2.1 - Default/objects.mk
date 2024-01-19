1968f7129ceaf5e4c18ec15c893703a222e86b97
Joel Braun <j6braun@uwaterloo.ca>
2024-01-18 20:43:52 -0500

    Initial gecko sdk addition and asset tag project addition

---
+++ Fob/FareFob/GNU ARM v12.2.1 - Default/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := C:/Users/Joel/SimplicityStudio/SDKs/gecko_sdk//protocol/bluetooth/lib/EFR32BG22/GCC/libbluetooth.a C:/Users/Joel/SimplicityStudio/SDKs/gecko_sdk//platform/emdrv/nvm3/lib/libnvm3_CM33_gcc.a C:/Users/Joel/SimplicityStudio/SDKs/gecko_sdk//platform/radio/rail_lib/autogen/librail_release/librail_efr32xg22_gcc_release.a
+
+LIBS := -lgcc -lc -lm -lnosys
+
---
+++ Fob/FareFob/GNU ARM v12.2.1 - Default/sources.mk
@@ -0,0 +1,53 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+S79_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+S79_UPPER_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+EXECUTABLES := 
+OBJS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+. \
+autogen \
+gecko_sdk_4.1.0/app/bluetooth/common/gatt_service_cte \
+gecko_sdk_4.1.0/app/bluetooth/common/gatt_service_cte_adv \
+gecko_sdk_4.1.0/app/bluetooth/common/in_place_ota_dfu \
+gecko_sdk_4.1.0/app/common/util/app_log \
+gecko_sdk_4.1.0/hardware/board/src \
+gecko_sdk_4.1.0/hardware/driver/mx25_flash_shutdown/src/sl_mx25_flash_shutdown_usart \
+gecko_sdk_4.1.0/platform/Device/SiliconLabs/EFR32BG22/Source \
+gecko_sdk_4.1.0/platform/bootloader/api \
+gecko_sdk_4.1.0/platform/bootloader/app_properties \
+gecko_sdk_4.1.0/platform/common/src \
+gecko_sdk_4.1.0/platform/common/toolchain/src \
+gecko_sdk_4.1.0/platform/driver/leddrv/src \
+gecko_sdk_4.1.0/platform/emdrv/nvm3/src \
+gecko_sdk_4.1.0/platform/emlib/src \
+gecko_sdk_4.1.0/platform/radio/rail_lib/plugin/pa-conversions \
+gecko_sdk_4.1.0/platform/radio/rail_lib/plugin/rail_util_pti \
+gecko_sdk_4.1.0/platform/service/device_init/src \
+gecko_sdk_4.1.0/platform/service/hfxo_manager/src \
+gecko_sdk_4.1.0/platform/service/iostream/src \
+gecko_sdk_4.1.0/platform/service/mpu/src \
+gecko_sdk_4.1.0/platform/service/power_manager/src \
+gecko_sdk_4.1.0/platform/service/sleeptimer/src \
+gecko_sdk_4.1.0/platform/service/system/src \
+gecko_sdk_4.1.0/platform/service/udelay/src \
+gecko_sdk_4.1.0/protocol/bluetooth/src \
+gecko_sdk_4.1.0/util/silicon_labs/silabs_core/memory_manager \
+gecko_sdk_4.1.0/util/third_party/crypto/mbedtls/library \
+gecko_sdk_4.1.0/util/third_party/crypto/sl_component/se_manager/src \
+gecko_sdk_4.1.0/util/third_party/crypto/sl_component/sl_cryptoacc_library/src \
+gecko_sdk_4.1.0/util/third_party/crypto/sl_component/sl_mbedtls_support/src \
+gecko_sdk_4.1.0/util/third_party/crypto/sl_component/sl_protocol_crypto/src \
+gecko_sdk_4.1.0/util/third_party/crypto/sl_component/sl_psa_driver/src \
+
