e41091d450fdbd5817933482f629f2e2dd713991
Jackson <jacksonenright2001@gmail.com>
2024-03-04 21:09:00 -0500

    reset the gui on stm board reset, add commented code to support two distance sensors

--- RaspberryPi/p_export_bt_host_positioning/app/bluetooth/example_host/bt_host_positioning_gui/fob_processing.py
+++ RaspberryPi/p_export_bt_host_positioning/app/bluetooth/example_host/bt_host_positioning_gui/fob_processing.py
@@ -6,13 +6,12 @@ import mqtt_client
 import protocol_serial
 import signal
 from threading import Lock
+import time
 import tkinter_gui
 from uuid import UUID
 
 BUS_NODE_WITH_CONNECTION_PORT = '/dev/ttyACM1' #outermost (triggered first upon entry)
-BUS_NODE_DISTANCE_SENSOR_ONLY_PORT = '/dev/ttyACM2' #innermost distance sensor (triggered first upon exit)
-
-
+# BUS_NODE_DISTANCE_SENSOR_ONLY_PORT = '/dev/ttyACM2' #innermost distance sensor (triggered first upon exit)
 
 gui = None
 fob_processing = None
@@ -20,16 +19,18 @@ fob_processing = None
 class FobProcessing:
   def __init__(self, gui):
     self.lock = Lock()
+    self.reset_variables()
+    self.gui = gui
+    self.mqtt = mqtt_client.MQTTClient(self)
+    self.pt_serial = protocol_serial.ProtocolSerial(BUS_NODE_WITH_CONNECTION_PORT, self.door_announcement_1, self.init_announcement) 
+    # self.pt_serial_distance = protocol_serial.ProtocolSerial(BUS_NODE_DISTANCE_SENSOR_ONLY_PORT, self.door_announcement_2, self.init_announcement) 
+
+  def reset_variables(self):
     self.in_cylinder = {}
     self.tags_being_fare_checked = []
     self.someone_in_doorway1 = False
-    self.someone_in_doorway2 = False
-
+    # self.someone_in_doorway2 = False
     self.processed_count = 0
-    self.gui = gui
-    self.mqtt = mqtt_client.MQTTClient(self)
-    self.pt_serial = protocol_serial.ProtocolSerial(BUS_NODE_WITH_CONNECTION_PORT, self.door_announcement, self.init_announcement) 
-    self.pt_serial_distance = protocol_serial.ProtocolSerial(BUS_NODE_DISTANCE_SENSOR_ONLY_PORT, self.door_announcement_2, self.init_announcement) 
 
   def is_point_inside_cylinder(self, x, y, z, radius, height):
     in_circle = x ** 2 + y ** 2 <= radius ** 2
@@ -41,15 +42,16 @@ class FobProcessing:
     with self.lock:
       if address in self.tags_being_fare_checked:
         self.tags_being_fare_checked.remove(address)
-    gui.enqueue_state(tkinter_gui.State.FAILURE if uuid == UUID(int=0) else tkinter_gui.State.SUCCESS)
+      self.gui.enqueue_state(tkinter_gui.State.FAILURE if uuid == UUID(int=0) else tkinter_gui.State.SUCCESS)
 
   def process_message(self, x, y, z, tag_id):
     #if not tag_id in currently_connected
     # this.pt_serial.send_request_connect(tag_id, get_connect)
-    is_tag_in_cylinder = self.is_point_inside_cylinder(x, y, z, 1, 10)
+    is_tag_in_cylinder = self.is_point_inside_cylinder(x, y, z, 10, 10)
     with self.lock:
       self.in_cylinder[tag_id] = (is_tag_in_cylinder, datetime.now())
-      if not self.someone_in_doorway:
+      #if not self.someone_in_doorway1 and not self.someone_in_doorway2:
+      if not self.someone_in_doorway1:
         return
       most_recent_in_cyl_id = None
       most_recent_in_cyl_time = None
@@ -64,45 +66,49 @@ class FobProcessing:
       self.pt_serial.send_request_fare(most_recent_in_cyl_id, lambda uuid: self.get_fare_id(most_recent_in_cyl_id, uuid))
       self.processed_count += 1
 
-  def door_announcement(self, inDoorway: bool):
+  def door_announcement_1(self, inDoorway: bool):
     print("Announcement inDoorway: " + str(inDoorway))
     with self.lock:
       self.someone_in_doorway1 = inDoorway
       if inDoorway:
-        #entry case
-        if self.someone_in_doorway2:
-          self.gui.increment_person_counter()
-        else:
-          print("ERROR: Both sensors are false on entry")
-
+        return
       if self.processed_count == 0:
         self.gui.enqueue_state(tkinter_gui.State.FAILURE, 'Valid payment fob not found.')
+        self.gui.increment_person_counter() #TEMP
       else:
         self.processed_count = 0
+      # if self.someone_in_doorway2:
+      #   self.gui.increment_person_counter()
 
-  def door_announcement_2(self, inDoorway: bool):
-    print("Announcement inDoorway: " + str(inDoorway))
-    with self.lock:
-      self.someone_in_doorway2 = inDoorway
-      if inDoorway:
-        if self.someone_in_doorway1:
-          self.gui.decrement_person_counter()
-        else:
-          print("ERROR: Both sensors are false on person exit")
-   
-
+  # def door_announcement_2(self, inDoorway: bool):
+  #   print("Announcement inDoorway2: " + str(inDoorway))
+  #   with self.lock:
+  #     if not self.gui:
+  #       return
+  #     self.someone_in_doorway2 = inDoorway
+  #     if inDoorway:
+  #       return
+  #     if self.someone_in_doorway1:
+  #       self.gui.decrement_person_counter()
 
   def init_announcement(self, flags: int):
     protocol_serial.init_printout(flags)
     self.pt_serial.send_request_connect('0C4314F4627F', self.get_connect) # TODO put this in on_message instead
+    self.pt_serial.send_request_connect('4C5BB3CA9C47', self.get_connect) # TODO put this in on_message instead
+    self.pt_serial.send_request_connect('4C5BB3CA9C43', self.get_connect) # TODO put this in on_message instead
+    with self.lock:
+      self.gui.reset()
+      self.reset_variables()
 
-  def get_connect(self):
+  def get_connect(self, state: int):
     print("huh") # TODO add to a list of current connections
 
 def signal_handler(sig, frame):
   gui.running = False
   #fob_processing.mqtt.running = False
   fob_processing.pt_serial.running = False
+  global running
+  running = False
 
 def main():
   # setup signal handler for graceful shutdown
@@ -113,8 +119,13 @@ def main():
   # setup fob_processing, mqtt client, and serial communication with BlueNRG board
   global fob_processing
   fob_processing = FobProcessing(gui)
-  # GUI blocking loop
-  gui.root.mainloop()
+  # start GUI loop on separate thread
+  gui.start_main_loop()
+  # loop on main thread until program termination
+  global running
+  while running:
+    print('running') # not printing currently??
+    time.sleep(0.5)
 
 if __name__ == "__main__":
   main()
--- RaspberryPi/p_export_bt_host_positioning/app/bluetooth/example_host/bt_host_positioning_gui/tkinter_gui.py
+++ RaspberryPi/p_export_bt_host_positioning/app/bluetooth/example_host/bt_host_positioning_gui/tkinter_gui.py
@@ -1,6 +1,7 @@
 from collections import deque, namedtuple
 from enum import Enum
 import threading
+from threading import Lock
 import time
 import tkinter
 
@@ -33,10 +34,8 @@ class TkinterGUI:
     # setup graceful shutdown
     self.running = True 
     # initialize person counter, system status, state and state queue handling thread
-    self.num_people_on_bus = 0
-    self.system_status = SystemStatus.RUNNING
-    self.state = State.WAITING
-    self.state_queue = deque()
+    self.lock = Lock()
+    self.reset(True)
     self.state_thread = threading.Thread(target=self.process_state_queue, daemon=True)
     self.state_thread.start()
     # initialize gui
@@ -51,7 +50,18 @@ class TkinterGUI:
     gui_bottom_rectangle = self.canvas.create_rectangle(lower_text_bounding_box[0]-WINDOW_PADDING_PX, lower_text_bounding_box[1]-WINDOW_PADDING_PX, WINDOW_WIDTH_PX, lower_text_bounding_box[3]+WINDOW_PADDING_PX, fill='#3B3B3B')                                 
     self.canvas.tag_lower(gui_bottom_rectangle, self.gui_person_counter_text)
     self.gui_main_text = self.canvas.create_text(WINDOW_WIDTH_PX/2, (WINDOW_HEIGHT_PX-(lower_text_bounding_box[3]-lower_text_bounding_box[1]+2*WINDOW_PADDING_PX))/2, text=self.state.value.default_message, font=(FONT, FONT_SIZE, FONT_WEIGHT), fill='black', anchor='center')
-    self.root.after(100, self.check_if_running)
+
+  def reset(self, init = False):
+    with self.lock:
+      self.num_people_on_bus = 0
+      self.system_status = SystemStatus.RUNNING
+      self.state = State.WAITING
+      self.state_queue = deque()
+      if not init:
+        self.canvas.itemconfig(self.gui_person_counter_text, text=self.get_person_counter_string())
+        self.canvas.itemconfig(self.gui_status_text, text=self.system_status.value.default_message)
+        self.canvas.itemconfig(self.gui_main_text, text=self.state.value.default_message)
+        self.canvas.config(bg=self.state.value.color)
 
   def check_if_running(self):
     if not self.running:
@@ -63,47 +73,60 @@ class TkinterGUI:
     return f'{PERSON_COUNTER_TEXT_TEMPLATE}{self.num_people_on_bus}'
 
   def increment_person_counter(self):
-    self.num_people_on_bus += 1
-    self.canvas.itemconfig(self.gui_person_counter_text, self.get_person_counter_string())
+    with self.lock:
+      self.num_people_on_bus += 1
+      self.canvas.itemconfig(self.gui_person_counter_text, text=self.get_person_counter_string())
 
   def decrement_person_counter(self):
-    if self.num_people_on_bus > 0:
-      self.num_people_on_bus -= 1
-    self.canvas.itemconfig(self.gui_person_counter_text, self.get_person_counter_string())
+    with self.lock:
+      if self.num_people_on_bus > 0:
+        self.num_people_on_bus -= 1
+      self.canvas.itemconfig(self.gui_person_counter_text, text=self.get_person_counter_string())
 
   def set_system_status(self, system_status):
-    self.system_status = system_status
-    self.canvas.itemconfig(self.gui_status_text, text=system_status.value.default_message)
+    with self.lock:
+      self.system_status = system_status
+      self.canvas.itemconfig(self.gui_status_text, text=system_status.value.default_message)
+
+  def enqueue_state(self, state, display_text=None):
+    with self.lock:
+      display_text = display_text if display_text is not None else state.value.default_message
+      self.state_queue.append(StateWithMessage(state=state, message=display_text))
+      # add waiting state to the queue for after failure or success state, to be shown after TIME_IN_SUCCESS_OR_FAILURE_STATE_S seconds
+      if state == State.SUCCESS or state == State.FAILURE:
+        self.state_queue.append(StateWithMessage(state=State.WAITING, message=State.WAITING.value.default_message))
 
   def process_state_queue(self):
     while self.running:
-      if not self.state_queue:
-        continue
-      state, display_text = None, None
-      # when in 'VALIDATING' state, ensure the result of the validation is the next state selected
-      if self.state == State.VALIDATING:
-        found_validation_result = False
-        for state_with_message in list(self.state_queue):
-          # only want to transition to a success or failure failure state related to the validation, not a failure state caused by someone walking through with no tag tag
-          if (state_with_message.state == State.SUCCESS or state_with_message.state == State.FAILURE) and state_with_message.message == state_with_message.state.value.default_message:
-            found_validation_result = True
-            state, display_text = state_with_message
-            self.state_queue.remove(state_with_message)
-            break
-        if not found_validation_result:
+      with self.lock:
+        if not self.state_queue:
           continue
-      else:
-        state, display_text  = self.state_queue.popleft()
-      self.state = state
-      background = state.value.color
-      self.canvas.config(bg=background)
-      self.canvas.itemconfig(self.gui_main_text, text=display_text)
-      if state == State.SUCCESS or state == State.FAILURE:
-        time.sleep(TIME_IN_SUCCESS_OR_FAILURE_STATE_S)
+        state, display_text = None, None
+        # when in 'VALIDATING' state, ensure the result of the validation is the next state selected
+        if self.state == State.VALIDATING:
+          found_validation_result = False
+          for state_with_message in list(self.state_queue):
+            # only want to transition to a success or failure failure state related to the validation, not a failure state caused by someone walking through with no tag tag
+            if (state_with_message.state == State.SUCCESS or state_with_message.state == State.FAILURE) and state_with_message.message == state_with_message.state.value.default_message:
+              found_validation_result = True
+              state, display_text = state_with_message
+              self.state_queue.remove(state_with_message)
+              break
+          if not found_validation_result:
+            continue
+        else:
+          state, display_text  = self.state_queue.popleft()
+        self.state = state
+        background = state.value.color
+        self.canvas.config(bg=background)
+        self.canvas.itemconfig(self.gui_main_text, text=display_text)
+        if state == State.SUCCESS or state == State.FAILURE:
+          time.sleep(TIME_IN_SUCCESS_OR_FAILURE_STATE_S)
 
-  def enqueue_state(self, state, display_text=None):
-    display_text = display_text if display_text is not None else state.value.default_message
-    self.state_queue.append(StateWithMessage(state=state, message=display_text))
-    # add waiting state to the queue for after failure or success state, to be shown after TIME_IN_SUCCESS_OR_FAILURE_STATE_S seconds
-    if state == State.SUCCESS or state == State.FAILURE:
-      self.state_queue.append(StateWithMessage(state=State.WAITING, message=State.WAITING.value.default_message))
+  def start_main_loop(self):
+    self.main_thread = threading.Thread(target=self.main_loop(), daemon=True)
+    self.main_thread.start()
+
+  def main_loop(self):
+    self.root.after(100, self.check_if_running)
+    self.root.mainloop()
