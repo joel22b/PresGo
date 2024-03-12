8edd3c5864edb6a3bc26eca67d1a82ab6193ed68
Jackson <jacksonenright2001@gmail.com>
2024-03-12 03:41:27 -0400

    not working currently

--- RaspberryPi/p_export_bt_host_positioning/app/bluetooth/example_host/bt_host_positioning_gui/fob_processing.py
+++ RaspberryPi/p_export_bt_host_positioning/app/bluetooth/example_host/bt_host_positioning_gui/fob_processing.py
@@ -5,9 +5,7 @@ from collections import deque, namedtuple
 from datetime import datetime
 import mqtt_client
 import protocol_serial
-import threading
 from threading import Lock
-import time
 import tkinter_gui
 from uuid import UUID
 import fare_system
@@ -21,7 +19,6 @@ LOCKOUT_VALIDATION_ATTEMPT_S = 15                # only attempt a validation aga
 LOCKOUT_NO_FOB_STATE_S = 0.4                     # only trigger no fare fob state (passing distance sensor with no fob) every x seconds
 LOCKOUT_PERSON_COUNTER_UPDATE_S = 0.75           # don't update the person counter again until x seconds have elapsed
 STALE_LOCATION_DATA_THRESHOLD_S = 1.5            # ignore saved position measurements from a fob if they are older than x seconds
-PROCESS_PAY_ZONE_QUEUE_INTERVAL_S = 0.1          # check for fobs in pay zone every x seconds
 
 IdWithTimeLastSeen = namedtuple('IdWithTimeLastSeen', ['id', 'time_last_seen'])
 
@@ -32,45 +29,49 @@ class FobProcessing:
     self.fare_sys = fare_system.FareSystem()
     self.gui = tkinter_gui.TkinterGUI(self)
     self.pt_serial = protocol_serial.ProtocolSerial(STM_BLUETOOTH_AND_DISTANCE_PORT, self.gui, self.on_door_announcement_front, self.on_stm_init_announcement, self.on_disconnect_announcement)
-    self.pt_serial_distance = protocol_serial.ProtocolSerial(STM_DISTANCE_ONLY_PORT, self.gui, self.on_door_announcement_back, self.on_stm_init_announcement)
+    self.pt_serial_distance = protocol_serial.ProtocolSerial(STM_DISTANCE_ONLY_PORT, self.gui, self.on_door_announcement_back, self.on_distance_stm_init_announcement)
     self.mqtt = mqtt_client.MQTTClient(self, self.gui)
     self.pt_serial.send_announcement_kill()
     self.pt_serial_distance.send_announcement_kill()
-    threading.Thread(target=self.process_pay_zone_queue, daemon=True).start()
 
   def process_message(self, x, y, z, fob_id):
-    with self.lock:
-      if fob_id not in self.connecting and fob_id not in self.connected:
-        self.pt_serial.send_request_connect(fob_id, lambda state: self.on_connect_announcement(state, fob_id))
-        self.connecting.append(fob_id)
-        #print("Initiating connection with:", fob_id)
-        #self.print_connect_lists()
-      #print('x', x, "y", y, self.is_point_inside_pay_zone(x, y, z))
-      if not self.is_point_inside_pay_zone(x, y, z):
+    if fob_id not in self.connecting and fob_id not in self.connected:
+      self.pt_serial.send_request_connect(fob_id, lambda state: self.on_connect_announcement(state, fob_id))
+      self.connecting.append(fob_id)
+      #print("Initiating connection with:", fob_id)
+      #self.print_connect_lists()
+    #print('x', x, "y", y, self.is_point_inside_pay_zone(x, y, z))
+    if not self.is_point_inside_pay_zone(x, y, z):
+      with self.lock:
+        self.pay_zone_queue = deque(fob for fob in self.pay_zone_queue if fob.id != fob_id)
+      return
+    for id_with_time_last_seen in self.pay_zone_queue:
+      if id_with_time_last_seen.id == fob_id:
         return
-      for id_with_time_last_seen in self.pay_zone_queue:
-        if id_with_time_last_seen.id == fob_id:
-          return
+    with self.lock:
+      print('add to q')
       self.pay_zone_queue.append(IdWithTimeLastSeen(id=fob_id, time_last_seen=datetime.now()))
 
-  def process_pay_zone_queue(self):
-    while True:
-      time.sleep(PROCESS_PAY_ZONE_QUEUE_INTERVAL_S)
-      with self.lock:
-        if not self.someone_in_doorway_front and not self.someone_in_doorway_back:
-          continue
-        if len(self.pay_zone_queue) == 0:
-          continue
-        (id, time_last_seen) = self.pay_zone_queue.pop()
-        if id in self.validating or \
-          id in self.last_validation_attempt and \
-          (datetime.now() - self.last_validation_attempt[id]).total_seconds() < LOCKOUT_VALIDATION_ATTEMPT_S or \
-          (datetime.now() - time_last_seen).total_seconds() > STALE_LOCATION_DATA_THRESHOLD_S:
-            continue
-        self.gui.enqueue_state(tkinter_gui.State.VALIDATING)
-        self.validating.append(id)
-        self.pt_serial.send_request_fare(id, lambda uuid: self.on_fare_response(id, uuid))
-        self.processed_count += 1
+  def process_pay_zone_queue_entry(self):
+    with self.lock:
+      len = len(self.pay_zone_queue)
+    if len:
+      if not self.last_no_fob_time or (datetime.now() - self.last_no_fob_time).total_seconds() > LOCKOUT_NO_FOB_STATE_S:
+        self.gui.enqueue_state(tkinter_gui.State.FAILURE, 'Valid Fare Fob not found.')
+      self.last_no_fob_time = datetime.now()
+      return
+    with self.lock:
+      (id, time_last_seen) = self.pay_zone_queue.pop()
+    print('test1')
+    if id in self.validating or \
+      id in self.last_validation_attempt and \
+      (datetime.now() - self.last_validation_attempt[id]).total_seconds() < LOCKOUT_VALIDATION_ATTEMPT_S or \
+      (datetime.now() - time_last_seen).total_seconds() > STALE_LOCATION_DATA_THRESHOLD_S:
+        print('return early')
+        return
+    self.gui.enqueue_state(tkinter_gui.State.VALIDATING)
+    self.validating.append(id)
+    self.pt_serial.send_request_fare(id, lambda uuid: self.on_fare_response(id, uuid))
 
   def on_fare_response(self, address: str, uuid: UUID):
     self.last_validation_attempt[address] = datetime.now()
@@ -88,48 +89,39 @@ class FobProcessing:
         self.validating.remove(address)
 
   def on_door_announcement_front(self, inDoorway: bool):
-    print("STM Announcement - front sensor inDoorway: " + str(inDoorway))
-    with self.lock:
-      self.someone_in_doorway_front = inDoorway
-      if inDoorway:
-        return
-      if self.someone_in_doorway_back and \
-        (self.last_person_counter_update_time is None or (datetime.now() - self.last_person_counter_update_time).total_seconds() > LOCKOUT_PERSON_COUNTER_UPDATE_S):
-          self.gui.increment_person_counter()
-          self.last_person_counter_update_time = datetime.now()
-          if self.processed_count == 0:
-            if not self.last_no_fob_time or (datetime.now() - self.last_no_fob_time).total_seconds() > LOCKOUT_NO_FOB_STATE_S:
-              self.gui.enqueue_state(tkinter_gui.State.FAILURE, 'Valid Fare Fob not found.')
-            self.last_no_fob_time = datetime.now()
-          else:
-            self.processed_count = 0
+    print("STM Announcement - Front sensor inDoorway: " + str(inDoorway))
+    self.someone_in_doorway_front = inDoorway
+    if inDoorway:
+      return
+    if self.someone_in_doorway_back and \
+      (self.last_person_counter_update_time is None or (datetime.now() - self.last_person_counter_update_time).total_seconds() > LOCKOUT_PERSON_COUNTER_UPDATE_S):
+        self.process_pay_zone_queue_entry()
+        self.gui.increment_person_counter()
+        self.last_person_counter_update_time = datetime.now()
  
   def on_door_announcement_back(self, inDoorway: bool):
     print("STM Announcement - Back sensor inDoorway: " + str(inDoorway))
-    with self.lock:
-      self.someone_in_doorway_back = inDoorway
-      if inDoorway:
-        return
-      if self.someone_in_doorway_front and \
-        (self.last_person_counter_update_time is None or(datetime.now() - self.last_person_counter_update_time).total_seconds() > LOCKOUT_PERSON_COUNTER_UPDATE_S):
-          self.gui.decrement_person_counter()
-          self.last_person_counter_update_time = datetime.now()
+    self.someone_in_doorway_back = inDoorway
+    if inDoorway:
+      return
+    if self.someone_in_doorway_front and \
+      (self.last_person_counter_update_time is None or(datetime.now() - self.last_person_counter_update_time).total_seconds() > LOCKOUT_PERSON_COUNTER_UPDATE_S):
+        self.gui.decrement_person_counter()
+        self.last_person_counter_update_time = datetime.now()
 
   def on_connect_announcement(self, state: int, address: str):
-    with self.lock:
-      if address in self.connecting:
-        self.connecting.remove(address)
-      if state == 0:
-        self.connected.append(address)
+    if address in self.connecting:
+      self.connecting.remove(address)
+    if state == 0:
+      self.connected.append(address)
     #print("STM Announcement - Connection state for " + address + ": " + str(state))
     #self.print_connect_lists()
 
   def on_disconnect_announcement(self, address: str):
-    with self.lock:
-      if address in self.connecting:
-        self.connecting.remove(address)
-      if address in self.connected:
-       self.connected.remove(address)
+    if address in self.connecting:
+      self.connecting.remove(address)
+    if address in self.connected:
+      self.connected.remove(address)
     #print("STM Announcement - Disconnected from: " + address)
     #self.print_connect_lists()
 
@@ -137,22 +129,27 @@ class FobProcessing:
     protocol_serial.init_printout(flags)
     self.pt_serial.check_and_set_error_status(flags)
     self.on_reset()
+  
+  def on_distance_stm_init_announcement(self, flags: int):
+    protocol_serial.init_printout(flags)
+    self.pt_serial_distance.check_and_set_error_status(flags)
+    self.on_reset()
 
   def initiate_reset(self):
     self.pt_serial.send_announcement_kill()
+    self.pt_serial_distance.send_announcement_kill()
 
   def on_reset(self):
-    with self.lock:
-      self.pay_zone_queue = deque()
-      self.last_validation_attempt = {}
-      self.validating = []
-      self.connecting = []
-      self.connected = []
-      self.someone_in_doorway_front = False
-      self.someone_in_doorway_back = False
-      self.processed_count = 0
-      self.last_no_fob_time = None
-      self.last_person_counter_update_time = None
+    self.pay_zone_queue = deque()
+    self.last_validation_attempt = {}
+    self.validating = []
+    self.connecting = []
+    self.connected = []
+    self.someone_in_doorway_front = False
+    self.someone_in_doorway_back = False
+    self.processed_count = 0
+    self.last_no_fob_time = None
+    self.last_person_counter_update_time = None
 
   def print_connect_lists(self):
     print('Fobs Connecting:', self.connecting)
--- RaspberryPi/p_export_bt_host_positioning/app/bluetooth/example_host/bt_host_positioning_gui/tkinter_gui.py
+++ RaspberryPi/p_export_bt_host_positioning/app/bluetooth/example_host/bt_host_positioning_gui/tkinter_gui.py
@@ -73,84 +73,77 @@ class TkinterGUI:
       self.set_system_status(SystemStatus.ERROR)
 
   def enqueue_state(self, state: State, display_text: str = ''):
-    with self.lock:
-      self.state_queue.append(StateWithMessage(state=state, message=state.value.default_message + display_text))
+    self.state_queue.append(StateWithMessage(state=state, message=state.value.default_message + display_text))
 
   def process_state_queue(self):
     try:
       while self.running:
-        with self.lock:
-          if len(self.state_queue) == 0:
-            continue
-          state, display_text = None, None
-          # when in 'VALIDATING' state, ensure the result of the validation is the next state selected
-          if self.state == State.VALIDATING:
-            found_validation_result = False
-            for state_with_message in list(self.state_queue):
-              # only want to transition to a success or failure failure state related to the validation, not a failure state caused by someone walking through with no tag
-              if (state_with_message.state == State.SUCCESS or state_with_message.state == State.FAILURE) and state_with_message.message != 'Valid payment fob not found.':
-                found_validation_result = True
-                state, display_text = state_with_message
-                self.state_queue.remove(state_with_message)
-                break
-            if not found_validation_result:
-              if (datetime.now() - self.validation_start_time).total_seconds() < VALIDATION_UI_TIMEOUT_S: # not 100% sure this timeout working
-                continue
-              # time in validation exceeds timeout, assume no there will be no response from stm board
-              self.validation_start_time = None
-              state = State.FAILURE
-              display_text = state.value.default_message
-          else:
-            state, display_text = self.state_queue.popleft()
-          self.state = state
-          background = state.value.color
-          self.canvas.config(bg=background)
-          self.canvas.itemconfig(self.main_text, text=display_text)
-          # add waiting state to the queue for after failure or success state, to be shown after TIME_IN_SUCCESS_OR_FAILURE_STATE_S seconds
-          if state == State.SUCCESS or state == State.FAILURE:
-            #os.system(f"aplay Sounds/{'success' if state == State.SUCCESS else 'failure'}.wav")
-            self.state_queue.append(StateWithMessage(state=State.WAITING, message=State.WAITING.value.default_message))
-            time.sleep(TIME_IN_SUCCESS_OR_FAILURE_STATE_S)
-          elif state == State.VALIDATING:
-            self.validation_start_time = datetime.now()
+        if len(self.state_queue) == 0:
+          continue
+        state, display_text = None, None
+        # when in 'VALIDATING' state, ensure the result of the validation is the next state selected
+        if self.state == State.VALIDATING:
+          found_validation_result = False
+          for state_with_message in list(self.state_queue):
+            # only want to transition to a success or failure failure state related to the validation, not a failure state caused by someone walking through with no tag
+            if (state_with_message.state == State.SUCCESS or state_with_message.state == State.FAILURE) and state_with_message.message != 'Valid payment fob not found.':
+              found_validation_result = True
+              state, display_text = state_with_message
+              self.state_queue.remove(state_with_message)
+              break
+          if not found_validation_result:
+            if (datetime.now() - self.validation_start_time).total_seconds() < VALIDATION_UI_TIMEOUT_S: # not 100% sure this timeout working
+              continue
+            # time in validation exceeds timeout, assume no there will be no response from stm board
+            self.validation_start_time = None
+            state = State.FAILURE
+            display_text = state.value.default_message
+        else:
+          state, display_text = self.state_queue.popleft()
+        self.state = state
+        background = state.value.color
+        self.canvas.config(bg=background)
+        self.canvas.itemconfig(self.main_text, text=display_text)
+        # add waiting state to the queue for after failure or success state, to be shown after TIME_IN_SUCCESS_OR_FAILURE_STATE_S seconds
+        if state == State.SUCCESS or state == State.FAILURE:
+          #os.system(f"aplay Sounds/{'success' if state == State.SUCCESS else 'failure'}.wav")
+          self.state_queue.append(StateWithMessage(state=State.WAITING, message=State.WAITING.value.default_message))
+          time.sleep(TIME_IN_SUCCESS_OR_FAILURE_STATE_S)
+        elif state == State.VALIDATING:
+          self.validation_start_time = datetime.now()
     except Exception as e:
       print('Error in tkinter_gui state queue loop:', str(e))
       self.set_system_status(SystemStatus.ERROR)
   
   def increment_person_counter(self):
-    with self.lock:
-      self.num_people_on_bus += 1
-      self.canvas.itemconfig(self.person_counter_text, text=self.get_person_counter_string())
+    self.num_people_on_bus += 1
+    self.canvas.itemconfig(self.person_counter_text, text=self.get_person_counter_string())
 
   def decrement_person_counter(self):
-    with self.lock:
-      if self.num_people_on_bus > 0:
-        self.num_people_on_bus -= 1
-      self.canvas.itemconfig(self.person_counter_text, text=self.get_person_counter_string())
+    if self.num_people_on_bus > 0:
+      self.num_people_on_bus -= 1
+    self.canvas.itemconfig(self.person_counter_text, text=self.get_person_counter_string())
 
   def get_person_counter_string(self):
     return f'{PERSON_COUNTER_TEXT_TEMPLATE}{self.num_people_on_bus}'
   
   def set_system_status(self, system_status):
-    with self.lock:
-      self.system_status = system_status
-      self.canvas.itemconfig(self.status_text, text=system_status.value.default_message, fill=system_status.value.color)
-      self.canvas.coords(self.status_template_text, self.canvas.bbox(self.status_text)[0], WINDOW_HEIGHT_PX-WINDOW_PADDING_PX)
+    self.system_status = system_status
+    self.canvas.itemconfig(self.status_text, text=system_status.value.default_message, fill=system_status.value.color)
+    self.canvas.coords(self.status_template_text, self.canvas.bbox(self.status_text)[0], WINDOW_HEIGHT_PX-WINDOW_PADDING_PX)
 
   def initiate_reset(self):
     self.on_reset()
-    with self.lock:
-      self.fob_processing.initiate_reset()
-      self.canvas.itemconfig(self.person_counter_text, text=self.get_person_counter_string())
-      self.canvas.itemconfig(self.status_text, text=self.system_status.value.default_message, fill=self.system_status.value.color)
-      self.canvas.coords(self.status_template_text, self.canvas.bbox(self.status_text)[0], WINDOW_HEIGHT_PX-WINDOW_PADDING_PX)
-      self.canvas.itemconfig(self.main_text, text=self.state.value.default_message)
-      self.canvas.config(bg=self.state.value.color)
+    self.fob_processing.initiate_reset()
+    self.canvas.itemconfig(self.person_counter_text, text=self.get_person_counter_string())
+    self.canvas.itemconfig(self.status_text, text=self.system_status.value.default_message, fill=self.system_status.value.color)
+    self.canvas.coords(self.status_template_text, self.canvas.bbox(self.status_text)[0], WINDOW_HEIGHT_PX-WINDOW_PADDING_PX)
+    self.canvas.itemconfig(self.main_text, text=self.state.value.default_message)
+    self.canvas.config(bg=self.state.value.color)
 
   def on_reset(self):
-    with self.lock:
-      self.num_people_on_bus = 0
-      self.system_status = SystemStatus.RUNNING
-      self.state = State.WAITING
-      self.validation_start_time = None
-      self.state_queue = deque()
+    self.num_people_on_bus = 0
+    self.system_status = SystemStatus.RUNNING
+    self.state = State.WAITING
+    self.validation_start_time = None
+    self.state_queue = deque()
