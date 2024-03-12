025ae7136d0c1b2d50df2351baba9ef43fc05128
Jackson <jacksonenright2001@gmail.com>
2024-03-12 07:18:52 -0400

    works nicely when only one fob is on

--- RaspberryPi/p_export_bt_host_positioning/app/bluetooth/example_host/bt_host_positioning_gui/fob_processing.py
+++ RaspberryPi/p_export_bt_host_positioning/app/bluetooth/example_host/bt_host_positioning_gui/fob_processing.py
@@ -35,46 +35,50 @@ class FobProcessing:
     self.pt_serial_distance.send_announcement_kill()
 
   def process_message(self, x, y, z, fob_id):
-    if fob_id not in self.connecting and fob_id not in self.connected:
+    with self.lock:
+      unconnected_fob = fob_id not in self.connecting and fob_id not in self.connected
+      if unconnected_fob:
+        self.connecting.append(fob_id)
+    if unconnected_fob:
       self.pt_serial.send_request_connect(fob_id, lambda state: self.on_connect_announcement(state, fob_id))
-      self.connecting.append(fob_id)
       #print("Initiating connection with:", fob_id)
       #self.print_connect_lists()
-    #print('x', x, "y", y, self.is_point_inside_pay_zone(x, y, z))
-    if not self.is_point_inside_pay_zone(x, y, z):
-      with self.lock:
-        self.pay_zone_queue = deque(fob for fob in self.pay_zone_queue if fob.id != fob_id)
       return
-    for id_with_time_last_seen in self.pay_zone_queue:
-      if id_with_time_last_seen.id == fob_id:
-        return
+    #print('x', x, "y", y, self.is_point_inside_pay_zone(x, y, z))
+    in_pay_zone = self.is_point_inside_pay_zone(x, y, z)
     with self.lock:
-      print('add to q')
-      self.pay_zone_queue.append(IdWithTimeLastSeen(id=fob_id, time_last_seen=datetime.now()))
+      for fob in self.pay_zone_queue:
+        if fob.id == fob_id:
+          if not in_pay_zone:
+            self.pay_zone_queue.remove(fob)
+          return
+      if in_pay_zone:
+        self.pay_zone_queue.append(IdWithTimeLastSeen(id=fob_id, time_last_seen=datetime.now()))
 
   def process_pay_zone_queue_entry(self):
+    no_fob_found = False
     with self.lock:
-      len = len(self.pay_zone_queue)
-    if len:
-      if not self.last_no_fob_time or (datetime.now() - self.last_no_fob_time).total_seconds() > LOCKOUT_NO_FOB_STATE_S:
-        self.gui.enqueue_state(tkinter_gui.State.FAILURE, 'Valid Fare Fob not found.')
-      self.last_no_fob_time = datetime.now()
-      return
+      if len(self.pay_zone_queue) == 0:
+        no_fob_found = not self.last_no_fob_time or (datetime.now() - self.last_no_fob_time).total_seconds() > LOCKOUT_NO_FOB_STATE_S
+    if no_fob_found:
+      self.gui.enqueue_state(tkinter_gui.State.FAILURE, 'Valid Fare Fob not found.')
+      with self.lock:
+        self.last_no_fob_time = datetime.now()
+        return
     with self.lock:
       (id, time_last_seen) = self.pay_zone_queue.pop()
-    print('test1')
-    if id in self.validating or \
-      id in self.last_validation_attempt and \
-      (datetime.now() - self.last_validation_attempt[id]).total_seconds() < LOCKOUT_VALIDATION_ATTEMPT_S or \
-      (datetime.now() - time_last_seen).total_seconds() > STALE_LOCATION_DATA_THRESHOLD_S:
-        print('return early')
-        return
-    self.gui.enqueue_state(tkinter_gui.State.VALIDATING)
-    self.validating.append(id)
+      if id in self.validating or \
+        id in self.last_validation_attempt and \
+        (datetime.now() - self.last_validation_attempt[id]).total_seconds() < LOCKOUT_VALIDATION_ATTEMPT_S or \
+        (datetime.now() - time_last_seen).total_seconds() > STALE_LOCATION_DATA_THRESHOLD_S:
+          return
+      self.validating.append(id)
+    self.gui.enqueue_state(tkinter_gui.State.VALIDATING)      
     self.pt_serial.send_request_fare(id, lambda uuid: self.on_fare_response(id, uuid))
 
   def on_fare_response(self, address: str, uuid: UUID):
-    self.last_validation_attempt[address] = datetime.now()
+    with self.lock:
+      self.last_validation_attempt[address] = datetime.now()
     print("Fare Response - Fare ID: " + str(uuid) + " Address: " + address)
     (valid, balance) = self.fare_sys.validate_fare(uuid)
     text = f"Remaining Balance: {'' if balance >= 0 else '-'}${str(abs(balance))}."
@@ -90,38 +94,48 @@ class FobProcessing:
 
   def on_door_announcement_front(self, inDoorway: bool):
     print("STM Announcement - Front sensor inDoorway: " + str(inDoorway))
-    self.someone_in_doorway_front = inDoorway
+    with self.lock:
+      self.someone_in_doorway_front = inDoorway
     if inDoorway:
       return
-    if self.someone_in_doorway_back and \
-      (self.last_person_counter_update_time is None or (datetime.now() - self.last_person_counter_update_time).total_seconds() > LOCKOUT_PERSON_COUNTER_UPDATE_S):
-        self.process_pay_zone_queue_entry()
-        self.gui.increment_person_counter()
+    with self.lock:
+      person_exiting_bus = self.someone_in_doorway_back and \
+        (self.last_person_counter_update_time is None or (datetime.now() - self.last_person_counter_update_time).total_seconds() > LOCKOUT_PERSON_COUNTER_UPDATE_S)
+    if person_exiting_bus:
+      self.process_pay_zone_queue_entry()
+      self.gui.increment_person_counter()
+      with self.lock:
         self.last_person_counter_update_time = datetime.now()
  
   def on_door_announcement_back(self, inDoorway: bool):
     print("STM Announcement - Back sensor inDoorway: " + str(inDoorway))
-    self.someone_in_doorway_back = inDoorway
+    with self.lock:
+      self.someone_in_doorway_back = inDoorway
     if inDoorway:
       return
-    if self.someone_in_doorway_front and \
-      (self.last_person_counter_update_time is None or(datetime.now() - self.last_person_counter_update_time).total_seconds() > LOCKOUT_PERSON_COUNTER_UPDATE_S):
-        self.gui.decrement_person_counter()
+    with self.lock:
+      person_entering_bus = self.someone_in_doorway_front and \
+        (self.last_person_counter_update_time is None or(datetime.now() - self.last_person_counter_update_time).total_seconds() > LOCKOUT_PERSON_COUNTER_UPDATE_S)
+    if person_entering_bus:
+      self.gui.decrement_person_counter()
+      with self.lock:
         self.last_person_counter_update_time = datetime.now()
 
   def on_connect_announcement(self, state: int, address: str):
-    if address in self.connecting:
-      self.connecting.remove(address)
-    if state == 0:
-      self.connected.append(address)
+    with self.lock:
+      if address in self.connecting:
+        self.connecting.remove(address)
+      if state == 0:
+        self.connected.append(address)
     #print("STM Announcement - Connection state for " + address + ": " + str(state))
     #self.print_connect_lists()
 
   def on_disconnect_announcement(self, address: str):
-    if address in self.connecting:
-      self.connecting.remove(address)
-    if address in self.connected:
-      self.connected.remove(address)
+    with self.lock:
+      if address in self.connecting:
+        self.connecting.remove(address)
+      if address in self.connected:
+        self.connected.remove(address)
     #print("STM Announcement - Disconnected from: " + address)
     #self.print_connect_lists()
 
@@ -140,21 +154,22 @@ class FobProcessing:
     self.pt_serial_distance.send_announcement_kill()
 
   def on_reset(self):
-    self.pay_zone_queue = deque()
-    self.last_validation_attempt = {}
-    self.validating = []
-    self.connecting = []
-    self.connected = []
-    self.someone_in_doorway_front = False
-    self.someone_in_doorway_back = False
-    self.processed_count = 0
-    self.last_no_fob_time = None
-    self.last_person_counter_update_time = None
+    with self.lock:
+      self.pay_zone_queue = deque()
+      self.last_validation_attempt = {}
+      self.validating = []
+      self.connecting = []
+      self.connected = []
+      self.someone_in_doorway_front = False
+      self.someone_in_doorway_back = False
+      self.last_no_fob_time = None
+      self.last_person_counter_update_time = None
 
   def print_connect_lists(self):
-    print('Fobs Connecting:', self.connecting)
-    print('Fobs Connected: ', self.connected)
+    with self.lock:
+      print('Fobs Connecting:', self.connecting)
+      print('Fobs Connected: ', self.connected)
 
   # pay zone is a rectangular prism
   def is_point_inside_pay_zone(self, x, y, z):
-    return x < PAY_ZONE_X_MAX and y < PAY_ZONE_Y_MAX and z < PAY_ZONE_Z_MAX
+    return abs(x) < PAY_ZONE_X_MAX and abs(y) < PAY_ZONE_Y_MAX and abs(z) < PAY_ZONE_Z_MAX
