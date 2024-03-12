70a46c24eb90a32de5798902eaebfd2dd062ff68
a73huang <itsandyhuang@gmail.com>
2024-03-11 21:32:59 -0400

    fixing directory

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Terminal 1
-lxterminal -e "bash -c 'cd PresGo/RaspberryPi/l_export_host_positioning/app/bluetooth/example_host/bt_aoa_host_locator; ./exe/bt_aoa_host_locator -u /dev/ttyACM0; exec bash'"
+lxterminal -e "bash -c 'cd ./RaspberryPi/l_export_host_positioning/app/bluetooth/example_host/bt_aoa_host_locator; ./exe/bt_aoa_host_locator -u /dev/ttyACM0; exec bash'"
 
 # Terminal 2
-lxterminal -e "bash -c 'cd PresGo/RaspberryPi/p_export_host_positioning/app/bluetooth/example_host/bt_host_positioning; ./exe/bt_host_positioning -c ./config/positioning_config.json; exec bash'"
+lxterminal -e "bash -c 'cd ./RaspberryPi/p_export_host_positioning/app/bluetooth/example_host/bt_host_positioning; ./exe/bt_host_positioning -c ./config/positioning_config.json; exec bash'"
 
 # Terminal 3
-lxterminal -e "bash -c 'cd PresGo/RaspberryPi/p_export_host_positioning/app/bluetooth/example_host/bt_host_positioning_gui; python3 main.py; exec bash'"
+lxterminal -e "bash -c 'cd ./RaspberryPi/p_export_host_positioning/app/bluetooth/example_host/bt_host_positioning_gui; python3 main.py; exec bash'"
